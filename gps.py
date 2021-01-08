b0c8ec16c3da7aecb43760aa7fdc76bd7727bf0c
pbabb2 <patrickbabbstem@gmail.com>
2021-01-07 21:14:36 -0800

    MSL altitude, automatic lat/long accounting for +-signs, # of satellites in view added

--- gps.py
+++ gps.py
@@ -6,6 +6,8 @@ See link below for sign conventions (Pos. Latitude = North, Negative Latitude = 
 Positive Longitude = East, Negative Longitude = West)
 
 http://academic.brooklyn.cuny.edu/geology/leveson/core/linksa/decimalconvert.html
+
+See link for GPGGA string information: https://raspberrypi.stackexchange.com/questions/12029/extracting-required-information-from-nmea-gps-data
 '''
 import serial               #import serial pacakge
 from time import sleep
@@ -19,13 +21,31 @@ def GPS_Info():
     nmea_time = []
     nmea_latitude = []
     nmea_longitude = []
+    nmea_MSL_altitude = []
+    nmea_MSL_altitude_unit = []
+    nmea_north_south_indicator = []
+    nmea_east_west_indicator = []
+    nmea_satellites = []
     nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
     nmea_latitude = NMEA_buff[1]                #extract latitude from GPGGA string
     nmea_longitude = NMEA_buff[3]               #extract longitude from GPGGA string
-    nmea_longitude = '-'+ nmea_longitude # If you live in the West of the Prime Meridian, keep the negative string
+    nmea_MSL_altitude = NMEA_buff[8]            #extract mean sea level altitude from GPGGA string
+    nmea_MSL_altitude_unit = NMEA_buff[9]       #extract altitude unit. ex. meters,M
+    nmea_north_south_indicator = NMEA_buff[2]   #if south of equator, latitude is negative
+    nmea_east_west_indicator = NMEA_buff[4]     #if west of prime meridian, longitude is negative
+    nmea_satellites = NMEA_buff[6]              #number of satellites in view
     
+    if nmea_north_south_indicator == 'S':
+        nmea_latitude = '-'+ nmea_latitude
+    
+    if nmea_east_west_indicator == 'W':
+        nmea_longitude = '-'+ nmea_longitude
+          
     print("NMEA Time: ", nmea_time,'\n')
-    print ("NMEA Latitude:", nmea_latitude,"NMEA Longitude:", nmea_longitude,'\n')
+    #print ("NMEA Latitude:", nmea_latitude,"NMEA Longitude:", nmea_longitude,'\n')
+    print("MSL altitude is:", nmea_MSL_altitude, nmea_MSL_altitude_unit, '\n')
+    print(nmea_north_south_indicator,'of equator',',',nmea_east_west_indicator,'of prime meridian','\n')
+    print(nmea_satellites , 'satellites in view','\n')
     
     lat = float(nmea_latitude)                  #convert string into float for calculation
     longi = float(nmea_longitude)               #convertr string into float for calculation
@@ -51,6 +71,8 @@ NMEA_buff = 0
 lat_in_degrees = 0
 long_in_degrees = 0
 
+
+
 try:
     while True:
         received_data = (str)(ser.readline())                   #read NMEA string received
@@ -64,6 +86,7 @@ try:
             map_link = 'http://maps.google.com/?q=' + lat_in_degrees + ',' + long_in_degrees    #create link to plot location on Google map
             print("<<<<<<<<press ctrl+c to plot location on google maps>>>>>>\n")               #press ctrl+c to plot on map and exit 
             print("------------------------------------------------------------\n")
+           
                         
 except KeyboardInterrupt:
     webbrowser.open(map_link)        #open current position information in google map
